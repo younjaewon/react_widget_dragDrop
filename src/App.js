7dbba222fa25a8f7b16002e25d94f73619230fd1
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-02-21 18:00:29 +0900

    drag and drop position css

--- src/App.js
+++ src/App.js
@@ -12,30 +12,56 @@ const menuData = [
 let posX = 0;
 let posY = 0;
 
+let originalX = 0;
+let originalY = 0;
+
 function App() {
   const [dragMenu, setDragMenu] = useState(null);
+
   const handleOnDragOver = (e) => {
     e.preventDefault();
   };
 
-  const handleOnDrop = (e) => {
-    const dropContent = e.target;
+  const handleOnDragStart = (e) => {
     posX = e.clientX;
     posY = e.clientY;
-    debugger;
-    dropContent.append(dragMenu);
-  };
 
-  const handleOnDragStart = (e) => {
+    originalX = e.target.offsetLeft;
+    originalY = e.target.offsetTop;
+
     let nodeCopy = e.target.cloneNode(true);
+    nodeCopy.style.left = posX;
+    nodeCopy.style.height = posY;
+    nodeCopy.style.position = "relative";
     setDragMenu(nodeCopy);
   };
 
+  const handleOnDrag = (e) => {
+    e.target.style.left = `${e.target.offsetLeft + e.clientX - posX}px`;
+    e.target.style.top = `${e.target.offsetTop + e.clientY - posY}px`;
+    posX = e.clientX;
+    posY = e.clientY;
+  };
+
+  const handleOnDragEnd = (e) => {};
+
+  const handleOnDrop = (e) => {
+    const dropContent = e.target;
+
+    dropContent.append(dragMenu);
+  };
+
   return (
-    <div style={{ display: "flex" }}>
+    <div style={{ display: "flex", position: "absolute" }}>
       <div style={{ width: "10%", border: "1px solid" }}>
         {menuData.map((menu) => (
-          <div key={menu.id} draggable onDragStart={handleOnDragStart}>
+          <div
+            key={menu.id}
+            draggable
+            onDragStart={handleOnDragStart}
+            onDrag={handleOnDrag}
+            onDragEnd={handleOnDragEnd}
+          >
             {menu.name}
           </div>
         ))}
@@ -43,7 +69,12 @@ function App() {
       <div
         onDragOver={handleOnDragOver}
         onDrop={handleOnDrop}
-        style={{ width: "100%", height: "600px", border: "1px solid" }}
+        style={{
+          width: "100%",
+          height: "600px",
+          border: "1px solid",
+          position: "absolute",
+        }}
       ></div>
     </div>
   );
--- src/canvancomponent/Menu1.js
+++ src/canvancomponent/Menu1.js
@@ -1,7 +1,52 @@
 import React, { useState } from "react";
 
-const Menu1 = () => {
-  return <div></div>;
+import "axui0datagrid/style.css";
+import { DataGrid } from "axui-datagrid";
+
+const Menu1 = (props) => {
+  const typeGroup = {
+    aTypes: ["A", "B", "C", "D"],
+    bTypes: ["A01", "A02", "B01", "B02", "C01", "C02"],
+    cTypes: ["Thomas", "Brant", "Ben", "Woo"],
+    priceTypes: [500, 1000, 1500, 2000],
+    amountTypes: [1, 2, 4, 5, 10, 20],
+    saleTypes: ["T", "B", "H", "W"],
+    saleDtTypes: [
+      "2018-01-20",
+      "2018-01-21",
+      "2018-02-01",
+      "2018-02-02",
+      "2018-02-03",
+    ],
+    customerTypes: ["장기영", "황인서", "양용성", "이하종", "김혜미", "홍시아"],
+  };
+
+  const getTypes = (typeName) => {
+    const types = typeGroup[typeName];
+    return types[Math.floor(Math.random() * types.length)];
+  };
+  return (
+    <>
+      <h1>Basic</h1>
+
+      <DataGrid
+        width={300}
+        height={300}
+        data={gridData}
+        columns={columns}
+        options={{
+          showLineNumber: true,
+          showRowSelector: true,
+          asidePanelWidth: 900,
+          columnKeys: { deleted: "D" },
+          header: {
+            clickAction: "sort",
+          },
+        }}
+        style={{ fontSize: "12px" }}
+      />
+    </>
+  );
 };
 
 export default Menu1;
